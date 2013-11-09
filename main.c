5d4e3df1b1a232c3126643387cd7f2abc095bf04
Nehmulos <nehmu@lavabit.com>
2013-11-09 15:52:00 +0100

    4space indentation;

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <ncurses.h>
 
 int main(int argc, const char* argv[]) {
-  for (int i=0; i < argc; ++i) {
-    printf("%s\n", argv[i]);
-  }
+    for (int i=0; i < argc; ++i) {
+	printf("%s\n", argv[i]);
+    }
   
-  initscr();
-  printw("Hello World!");
-  refresh();
-  getch();
-  endwin();
+    initscr();
+    printw("Hello World!");
+    refresh();
+    char c = getch();
+    putchar(c);
+    endwin();
 
-  return 0;
+    return 0;
 }
