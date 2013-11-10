b4fad4d986fcaeff310c9437a7b370c35e2392a0
Nehmulos <nehmu@lavabit.com>
2013-11-10 15:00:36 +0100

    fix argument names and store bufferUsed size (this fixes the json parsing bug);

--- CurlResult.c
+++ CurlResult.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <ncurses.h>
+#include <stdio.h>
 #include "CurlResult.h"
 
 void CurlResult_init(struct CurlResult* this) {
@@ -12,14 +13,14 @@ void CurlResult_destroyMembers(struct CurlResult* this) {
     free(this->buffer);
 }
 
-size_t CurlResult_write(void* buffer, size_t bufferSize, size_t nmemb, void* userdata) {
+size_t CurlResult_write(void* buffer, size_t charSize, size_t bufferSize, void* userdata) {
     if (!userdata) {
 	return 0;
     }
     struct CurlResult* this = userdata;
-    size_t newSize = this->bufferUsed + (bufferSize*nmemb);
+    size_t newSize = this->bufferUsed + (bufferSize*charSize);
     if (newSize > this->bufferAllocated) {
-	size_t newAllocSize = this->bufferAllocated + (bufferSize*nmemb);
+	size_t newAllocSize = this->bufferAllocated + (bufferSize*charSize);
 	char* newBuffer = malloc(newAllocSize);
 	memcpy(newBuffer, this->buffer, this->bufferUsed);
 	free(this->buffer);
@@ -28,15 +29,18 @@ size_t CurlResult_write(void* buffer, size_t bufferSize, size_t nmemb, void* use
     }
     int offset = this->bufferUsed;
     offset = 0 == offset ? 0 : offset -1;
-    memcpy(this->buffer + offset, buffer, (bufferSize*nmemb));
+    memcpy(this->buffer + offset, buffer, (bufferSize*charSize));
+    this->bufferUsed = this->bufferUsed + (bufferSize*charSize);
     return bufferSize;
 }
 
 json_value* CurlResult_parse(struct CurlResult* this) {
+    FILE* f = fopen("jsonbuffer", "w");
+    fwrite(this->buffer, sizeof(char), this->bufferUsed, f);
     char error[512];
     //json_settings settings;
     json_value* ret = json_parse_ex(NULL, this->buffer, this->bufferUsed, error);
-    printw("error: %s\n", error);
+    //    printw("error: %s\n", error);
     return ret;
 }
 
