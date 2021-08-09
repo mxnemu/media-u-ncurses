5136268a55cdb6198fd66e9e4e99544f30d7cdc5
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-10 00:26:01 +0800

    Delete Colors.c

--- Colors.c
+++
@@ -1,9 +0,0 @@
-#include "Colors.h"
-
-void Colors_init() {
-    init_pair(Colors_Default, COLOR_WHITE, COLOR_BLACK);
-    attron(COLOR_PAIR(Colors_Default));
-
-    init_pair(Colors_Selected, COLOR_YELLOW, COLOR_BLACK);
-    attron(COLOR_PAIR(Colors_Selected));
-}
